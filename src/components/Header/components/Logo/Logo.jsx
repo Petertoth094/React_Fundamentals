3b01a91f9185a2cd623bc5d112421500dd20026c
Peter Toth9 <peter_toth9@epam.com>
2021-10-27 08:52:12 +0200

    no need for the return statement: in logo comp

--- src/components/Header/components/Logo/Logo.jsx
+++ src/components/Header/components/Logo/Logo.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import './logo.css';
 
-const Logo = ({ logo }) => {
-	return (
-		<div className='img-container'>
-			<img src={logo} alt='courses-app-logo' className='photo' />
-		</div>
-	);
-};
+const Logo = ({ logo }) => (
+	<div className='img-container'>
+		<img src={logo} alt='courses-app-logo' className='photo' />
+	</div>
+);
 
 export default Logo;
